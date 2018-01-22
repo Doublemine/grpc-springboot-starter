d1cc675b74cc34a9be0452b7167e37470cca2457
Doublemine <doublemine.w@gmail.com>
2018-01-22 15:34:04 +0800

    update kotlin version

--- sample/src/main/java/work/wanghao/kotlin/boot/grpc/sample/controller/HelloController.java
+++ sample/src/main/java/work/wanghao/kotlin/boot/grpc/sample/controller/HelloController.java
@@ -1,5 +1,6 @@
 package work.wanghao.kotlin.boot.grpc.sample.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +15,14 @@ import work.wanghao.kotlin.boot.grpc.sample.service.GrpcClientService;
 @RestController
 public class HelloController {
 
-    @Autowired
-    private GrpcClientService grpcClientService;
+	@Autowired
+	private GrpcClientService grpcClientService;
 
-    @RequestMapping("/")
-    public String printMessage(@RequestParam(defaultValue = "Michael") String name) {
-        return grpcClientService.sendMessage(name);
-    }
+	@Autowired
+	private HttpServletRequest httpServletRequest;
+
+	@RequestMapping("/")
+	public String printMessage(@RequestParam(defaultValue = "Michael") String name) {
+		return grpcClientService.sendMessage(name);
+	}
 }
